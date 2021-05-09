ed883c895e913a0e3146edc31d3d320a7e8bd3df
Fangyuan Yu <66006349+fangyuan-ksgk@users.noreply.github.com>
2021-05-09 16:34:26 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+tod = torch.distributions
+
+from setuptools import setup
+setup(name='condpf',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='ksgk',
+author_email='fangyuan.yu@kaust.edu.sa',
+license='MIT',
+packages=['condpf'],
+zip_safe=False)
